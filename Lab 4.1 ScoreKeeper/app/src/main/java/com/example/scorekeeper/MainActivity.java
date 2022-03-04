eafc98541f78a8b249b96681fd2fe62b9514017b
HarshdeepKaurDhunna <97932988+HarshdeepKaurDhunna@users.noreply.github.com>
2022-03-04 02:00:43 -0500

    Add on click listener for buttons instead of multiple

--- Lab 4.1 ScoreKeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ Lab 4.1 ScoreKeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -12,73 +12,108 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Intailize the value
+    //Declare variables
     TextView scoreTeamOne;
     TextView scoreTeamTwo;
-    RadioGroup radioGroup;
-    RadioButton radioButton;
+
+    RadioButton radioBtnOne;
+    RadioButton radioBtnTwo;
+    RadioButton radioBtnThree;
+
     Button btnAddA;
     Button btnAddB;
     Button btnSubA;
     Button btnSubB;
-    int teamAScore;
-    int teamBScore;
+
+    int teamAScore = 0;
+    int teamBScore = 0;
+    int intVal = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addAListenerOnButton();
-        addBListenerOnButton();
+        //Initialize TextView by views in xml file
+        scoreTeamOne = findViewById(R.id.scoreTeamOne);
+        scoreTeamTwo = findViewById(R.id.scoreTeamTwo);
+
+        //Initialize TextView by views in xml file
+        radioBtnOne = findViewById(R.id.radioButtonIncreaseOne);
+        radioBtnTwo = findViewById(R.id.radioButtonIncreaseTwo);
+        radioBtnThree = findViewById(R.id.radioButtonIncreaseThree);
+
+        //Initialize TextView by views in xml file
+        btnAddA = findViewById(R.id.buttonAddA);
+        btnAddB = findViewById(R.id.buttonAddB);
+        btnSubA = findViewById(R.id.buttonSubA);
+        btnSubB = findViewById(R.id.buttonSubB);
+
+        //Initialize TextView by views in xml file
+        btnAddA.setOnClickListener(onClickListener);
+        btnAddB.setOnClickListener(onClickListener);
+        btnSubA.setOnClickListener(onClickListener);
+        btnSubB.setOnClickListener(onClickListener);
 
     }
 
-    public void addAListenerOnButton() {
-        btnAddA = (Button) findViewById(R.id.buttonAddA);
-        btnAddA.setOnClickListener(new OnClickListener() {
+    /**
+     * OnclickListner for all four buttons
+     * Switch case to add functionality on buttons
+     */
+    private OnClickListener onClickListener = new OnClickListener() {
         @Override
-        public void onClick(View view) {
-            scoreTeamOne = findViewById(R.id.scoreTeamOne);
-            scoreTeamOne.setText("New Score: " + String.valueOf(teamAScore));
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.buttonAddA:
+                    teamAScore = teamAScore + intVal;
+                    scoreTeamOne.setText("New Score: " + String.valueOf(teamAScore));
+                    break;
+                case R.id.buttonAddB:
+                    teamBScore = teamBScore + intVal;
+                    scoreTeamTwo.setText("New Score: " + String.valueOf(teamBScore));
+                    break;
+                case R.id.buttonSubA:
+                    teamAScore = teamAScore - intVal;
+                    scoreTeamOne.setText("New Score: " + String.valueOf(teamAScore));
+                    break;
+                case R.id.buttonSubB:
+                    teamBScore = teamBScore - intVal;
+                    scoreTeamTwo.setText("New Score: " + String.valueOf(teamBScore));
+                    break;
             }
 
-        });
+        }
+    };
 
-    }
-    public void addBListenerOnButton() {
-        btnAddB = (Button) findViewById(R.id.buttonAddB);
-        btnAddB.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                scoreTeamTwo = findViewById(R.id.scoreTeamTwo);
-                scoreTeamTwo.setText("hello1");
-            }
-
-        });
-
-    }
-
-    //Method to Get Selected Radio Button Value
-    public int onRadioButtonClicked(View view) {
+    /**
+     * Method to get Radio Button value onclick as define in xml
+     * @param view
+     */
+    public void onSelectRadioButton(View view) {
         boolean checked = ((RadioButton) view).isChecked();
 
         //Get the selected Radio Button Id
         switch(view.getId()) {
             case R.id.radioButtonIncreaseOne:
                 if (checked)
-                    teamAScore = teamAScore + 1;
+                    radioBtnTwo.setChecked(false);
+                    radioBtnThree.setChecked(false);
+                    intVal = 1;
                 break;
             case R.id.radioButtonIncreaseTwo:
                 if (checked)
-                    teamAScore = teamAScore + 2;
+                    radioBtnOne.setChecked(false);
+                    radioBtnThree.setChecked(false);
+                    intVal = 2;
                 break;
             case R.id.radioButtonIncreaseThree:
                 if (checked)
-                    teamAScore = teamAScore + 3;
+                    radioBtnOne.setChecked(false);
+                    radioBtnTwo.setChecked(false);
+                    intVal = 3;
                 break;
         }
-        return teamAScore;
     }
 
 
